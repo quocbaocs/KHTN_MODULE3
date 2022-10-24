1d253f270faf21e48ce447cce85d436ad4cbd75b
quocbaole <quocbaole368@gmail.com>
2022-10-24 20:44:03 +0700

    them thu vien hibernate

--- PhuongPerfumeM3/src/main/java/filters/VdFilter1.java
+++ PhuongPerfumeM3/src/main/java/filters/VdFilter1.java
@@ -9,33 +9,45 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebFilter("/VdFilter1")
 public class VdFilter1 implements Filter {
 
-  
-    public VdFilter1() {
-      
-    }
+	public VdFilter1() {
 
-	
+	}
+
 	public void destroy() {
-		
+
 	}
 
-	
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		//lay pathinfo: request.getPathInfo() khong co
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// lay pathinfo: request.getPathInfo() khong co
 		HttpServletRequest req = (HttpServletRequest) request;
+		
 		String pathInfo = req.getPathInfo();
-		System.out.println("PathInfo la: " +pathInfo);
+		System.out.println("PathInfo la: " + pathInfo);
+		
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		HttpSession session = req.getSession();// lay thong tin dang nhap dung session
+		
+		String uri = req.getRequestURI();
+		System.out.println("Trang goi den  : " + uri);
+		if(uri.equals("/PhuongPerfumeM3/trang-cua-hang.jsp")) {
+			resp.sendRedirect("trang-chu.jsp");
+		}else {
+
+		
 		chain.doFilter(request, response);
+		}
 	}
 
-	
 	public void init(FilterConfig fConfig) throws ServletException {
-		
+
 	}
 
 }
